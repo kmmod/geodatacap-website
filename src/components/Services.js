c67b4fbd2e37f71b014684b2ce7c597974f7a1ce
km-G5 <modzelewski.kamil@gmail.com>
2020-08-22 20:06:05 +0200

    Services WIP

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,55 +2,87 @@ import React from "react"
 import { Container, Section } from "./Structure"
 import ServicesBg from "./ServicesBg"
 import Fade from "react-reveal/Fade"
+import { AiOutlineBulb, AiOutlineDatabase, AiOutlineShoppingCart, AiOutlineLaptop, AiOutlinePartition } from "react-icons/ai"
+
 import services from "../styles/services.module.css"
 import fonts from "../styles/fonts.module.css"
-import tempimage from "../images/gatsby-icon.png"
 
-function Services() {
-  const databox = (
-    <React.Fragment>
-      <div className={services.breakline} />
+const DataBox = props => {
+  return (
+    <div className={services.container}>
+      {props.breakline ? <div className={services.breakline} /> : null}
 
-      <img src={tempimage} alt={""} />
+      {props.icon ? props.icon : null}
 
       <div className={fonts.header}>
-        <h4>Data preparation</h4>
+        <h4>{props.header}</h4>
       </div>
       <div className={fonts.body}>
-        <p>
-          Our consultants opt in to the projects they genuinely want to work on.
-        </p>
+        <p>{props.content}</p>
       </div>
-    </React.Fragment>
+    </div>
   )
+}
 
+function Services() {
   return (
     <Section>
       <ServicesBg />
       <Container>
         <div id={"home"} />
-        <div className={services.wrapper}>
-          <Fade clear cascade>
-            <div className={services.container}>
-              <div className={fonts.header}>
-                <h4>
+        <Fade clear cascade>
+          <div className={services.wrapper}>
+            <DataBox
+              breakline={false}
+              header={
+                <>
                   Digital agency <b>services & solutions</b>
-                </h4>
-              </div>
-              <div className={fonts.body}>
-                <p>
-                  Our consultants opt in to the projects they genuinely want to
-                  work on
-                </p>
-              </div>
-            </div>
-            <div className={services.container}>{databox}</div>
-            <div className={services.container}>{databox}</div>
-            <div className={services.container}>{databox}</div>
-            <div className={services.container}>{databox}</div>
-            <div className={services.container}>{databox}</div>
-          </Fade>
-        </div>
+                </>
+              }
+              content={
+                "Our consultants opt in to the projects they genuinely want to work on"
+              }
+            />
+            <DataBox
+              breakline={true}
+              icon={<AiOutlineDatabase />}
+              header={"Data Preparation"}
+              content={"Prepare your data to make the most of AI algorithms."}
+            />
+            <DataBox
+              breakline={true}
+              icon={<AiOutlineBulb />}
+              header={"AI Development"}
+              content={
+                "Find meaningful insight to improve your product or service"
+              }
+            />
+            <DataBox
+              breakline={true}
+              icon={<AiOutlineShoppingCart />}
+              header={"Digital Ecommerce"}
+              content={
+                "Create a personalized experience for every user based on real world data."
+              }
+            />
+            <DataBox
+              breakline={true}
+              icon={<AiOutlineLaptop />}
+              header={"Consumer technology"}
+              content={
+                "Find meaningful insight to improve your product or service"
+              }
+            />
+            <DataBox
+              breakline={true}
+              icon={<AiOutlinePartition />}
+              header={"Big Data Service"}
+              content={
+                "Build natural interactions with your users and identify patterns in unstructured data."
+              }
+            />
+          </div>
+        </Fade>
       </Container>
     </Section>
   )
